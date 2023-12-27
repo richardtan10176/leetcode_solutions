3e23621b3a718a78798d804b48a0e7fc6c9030e1
Richard Tan <richardtan787@gmail.com>
2023-12-27 18:05:32 -0500

    changed up organization format

--- c c++/ECE244/hello.cpp
+++
@@ -1,9 +0,0 @@
-//hello.cpp
-#include <iostream>
-using namespace std;
-int main(){
-    string name = "Richard";
-    cout << "Hello ";
-    cout << name << endl;
-    return 0;
-}
--- leetcodecompiledproblems/arrays/rotate_array.cpp
+++ leetcodecompiledproblems/arrays/rotate_array.cpp
@@ -5,6 +5,26 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
+        int rotations = k % nums.size();
+        int shifted[rotations];
+        int original[nums.size()-rotations];
+
+        for(int x = 0; x < nums.size(); x++){
+            if(x < rotations){
+                original[x] = nums[x];
+            }
+            else{
+                shifted[x-rotations] = nums[x];
+            }
+        }
+        for(int x = 0; x < nums.size(); x++){
+            if(x < rotations){
+                nums[x] = shifted[x];
+            }
+            else{
+                nums[x] = original[x-rotations];
+            }
+        }
     }
+
 };
