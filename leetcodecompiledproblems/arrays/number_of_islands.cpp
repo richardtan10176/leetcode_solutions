2ce2e2deeec2d03effb3dc8f571a003fed97968c
Richard Tan <richardtan787@gmail.com>
2023-12-31 16:54:31 -0500

    Create number_of_islands.cpp

---
+++ leetcodecompiledproblems/arrays/number_of_islands.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int height = grid.size();
+        int width = grid[0].size();
+
+        for(int y = 0; y < grid.size(); y++){
+            for(int x = 0; x < grid.size(); x++){
+                
+            }
+        }
+    }
+};
