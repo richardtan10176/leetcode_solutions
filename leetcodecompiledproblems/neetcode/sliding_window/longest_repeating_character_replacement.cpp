7ce741acf68d4a93df58829265f0a56ef1bc2925
Richard Tan <richardtan787@gmail.com>
2024-05-22 12:24:01 -0400

    added sliding window solution

--- leetcodecompiledproblems/neetcode/sliding_window/longest_repeating_character_replacement.cpp
+++ leetcodecompiledproblems/neetcode/sliding_window/longest_repeating_character_replacement.cpp
@@ -9,6 +9,22 @@
 class Solution {
 public:
     int characterReplacement(std::string s, int k) {
-        
+        int maxLength = 0;
+        int maxCount = 0; 
+        int start = 0;
+        int charCount[26] = {0}; 
+
+        for (int x = 0; x < s.size(); x++) {
+            maxCount = std::max(maxCount, ++charCount[s[x] - 'A']);
+
+            if (x - start + 1 - maxCount > k) {
+                charCount[s[start] - 'A']--;
+                start++;
+            }
+
+            maxLength = std::max(maxLength, x - start + 1);
+        }
+
+        return maxLength;
     }
 };
