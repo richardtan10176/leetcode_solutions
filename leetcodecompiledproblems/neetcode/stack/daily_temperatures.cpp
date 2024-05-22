def26a595cda484ca5c27fbe23130e387757a9fc
Richard Tan <richardtan787@gmail.com>
2024-05-22 18:30:50 -0400

    added stack solution

---
+++ leetcodecompiledproblems/neetcode/stack/daily_temperatures.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
+#include <stack>
+
+class Solution {
+public:
+    std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
+        int n = temperatures.size();
+        std::vector<int> answer(n, 0);
+        std::stack<int> s;
+        
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                int idx = s.top();
+                s.pop();
+                answer[idx] = i - idx;
+            }
+            s.push(i);
+        }
+        
+        return answer;
+    }
+};
