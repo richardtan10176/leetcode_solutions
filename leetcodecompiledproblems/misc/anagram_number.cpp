a9a7bc5ec806c1622451584f74f55e915c6a5549
Richard Tan <richardtan787@gmail.com>
2024-06-19 19:24:05 -0400

    added misc solution

--- leetcodecompiledproblems/misc/anagram_number.cpp
+++ leetcodecompiledproblems/misc/anagram_number.cpp
@@ -6,6 +6,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        std::string s = std::to_string(x);
+        int p1 = 0;
+        int p2 = s.size()-1;
+        while(p2 >= p1){
+            if(s[p2] != s[p1]){
+                return false;
+            }
+            p2--;
+            p1++;
+        }
+        return true;
     }
 };
