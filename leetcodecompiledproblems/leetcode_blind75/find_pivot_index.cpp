7ddd8d361d9de48ed1fbed1c218fa8850731d869
Richard Tan <richardtan787@gmail.com>
2024-08-29 15:57:42 -0400

    added blind75 solution

---
+++ leetcodecompiledproblems/leetcode_blind75/find_pivot_index.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> prefixSums; 
+        int sum = 0;
+        for(int x = n-1; x >= 0; x--){
+            prefixSums[x] = sum;
+            sum += nums[x];
+        }
+        int curSum = 0;
+        for(int x = 0;x < n; x++){
+            if(prefixSums[x] == curSum){
+                return x;   
+            }
+            curSum += nums[x];
+            
+        }
+        return -1;
+        
+    }
+};
