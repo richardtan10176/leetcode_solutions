b3fc754cd60aab9ef862924ee7233af2591a3ca9
Richard Tan <richardtan787@gmail.com>
2024-09-12 19:52:37 -0400

    added blind75 solution

---
+++ leetcodecompiledproblems/leetcode_blind75/odd_even_linked_list.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <ctype.h>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <stdlib.h>
+#include <ctype.h>
+using namespace std;
+
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+        
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even; 
+        
+        while (even && even->next) {
+            odd->next = even->next; 
+            odd = odd->next;        
+            
+            even->next = odd->next; 
+            even = even->next;      
+        }
+        
+        odd->next = evenHead;
+        
+        return head;
+    }
+};
+
